932ec4e1c9fa11c3791c6765b3da83a3d2e5f3ce
BluSkytop <olgakutnja@gmail.com>
2024-10-30 22:10:26 +0200

    add object and arr

--- .idea/lesson2.js
+++ .idea/lesson2.js
@@ -33,3 +33,64 @@ console.log(parseFloat('502010.1258'));//parseFloat відображає з пл
 console.log(!!502010);// !! - приведення до булевого значення//
 console.log(500 + true);// + true - це +1 //
 console.log(500 + false);// + false - це +0 //
+
+// обєкти //
+let dog = {
+    id:1,
+    name:'kokos',
+    bread:'spaniel',
+    age:3
+}
+console.log(dog);
+console.log(dog.age);
+
+// масив //
+let dogMas = [
+    {id:1, name:'chery', age:3},
+    {id:2, name:'cory', age:10},
+    {id:3, name:'chef', age:1},
+    {id:4, name:'cry', age:5},
+];
+console.log(dogMas.length);
+console.log(dogMas[0]);
+console.log(dogMas[1]);
+console.log(dogMas[2]);
+console.log(dogMas[3]);
+console.log(dogMas[3].age);
+console.log(dogMas[3].name);
+// витягаючи обєкт з масива його можна запакувати в будь-яку змінну. Приклад//
+let dog1 = dogMas[2];
+console.log(dog1.name);
+
+// обєднання двох обєктів з масивами даних в один масив//
+let cat = [
+    {id:11, name:'chaca', age:2},
+    {id:22, name:'rosa', age:8},
+    {id:33, name:'kusa', age:11},
+    {id:44, name:'snow', age:4},
+];
+let cat2 = [
+    {id:111, name:'sem', age:18},
+    {id:222, name:'kamila', age:7},
+    {id:333, name:'big', age:14},
+    {id:444, name:'semmi', age:8},
+];
+let multiArr = [
+    cat,
+    cat2
+];
+console.log(multiArr);
+console.log(multiArr.length);
+console.log(multiArr[1][0].name);
+
+// як вивести дані з масиву, який знаходиться в пачці даних обєкту загального масиву //
+let devs = [
+    {id:1001, name:'Kostja', skills:['js', 'node', 'html']},
+    {id:1002, name:'Mischa', skills:['html', 'css']},
+    {id:1003, name:'Orest', skills:['java', 'piton', 'html']},
+    {id:1004, name:'Katja', skills:['html', 'js','node', 'react']},
+];
+console.log(devs);
+console.log(devs[1].skills[0]);
+console.log(devs[1].skills[0]);
+console.log(devs.length);
